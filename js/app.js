5c42ddb30a802ecb225ae353adc77b8be2abe6f5
Katy Bowers <katyvine@gmail.com>
2018-03-19 17:18:41 -0700

    total click counter built

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// array to hold pic info
+
 Pic.allPictures = [];
 Pic.currentPictures = [];
 
@@ -22,20 +22,20 @@ new Pic('img/bathroom.jpg', 'bathroom');
 new Pic('img/boots.jpg', 'boots');
 new Pic('img/breakfast.jpg', 'breakfast');
 new Pic('img/bubblegum.jpg', 'bubblegum');
-// new Pic('img/chair.jpg', 'chari');
-// new Pic('img/cthulhu.jpg', 'cthulhu');
-// new Pic('img/dog-duck.jpg', 'dog-duck');
-// new Pic('img/dragon.jpg', 'dragon');
-// new Pic('img/pen.jpg', 'pen');
-// new Pic('img/pet-sweep.jpg', 'pet-sweep');
-// new Pic('img/scissors.jpg', 'scissors');
-// new Pic('img/shark.jpg', 'shark');
-// new Pic('img/sweep.png', 'sweep');
-// new Pic('img/tauntaun.jpg', 'tauntaun');
-// new Pic('img/unicorn.jpg', 'unicorn');
-// new Pic('img/usb.gif', 'usb');
-// new Pic('img/water-can.jpg', 'water-can');
-// new Pic('img/wine-glass.jpg', 'wine-glass');
+new Pic('img/chair.jpg', 'chari');
+new Pic('img/cthulhu.jpg', 'cthulhu');
+new Pic('img/dog-duck.jpg', 'dog-duck');
+new Pic('img/dragon.jpg', 'dragon');
+new Pic('img/pen.jpg', 'pen');
+new Pic('img/pet-sweep.jpg', 'pet-sweep');
+new Pic('img/scissors.jpg', 'scissors');
+new Pic('img/shark.jpg', 'shark');
+new Pic('img/sweep.png', 'sweep');
+new Pic('img/tauntaun.jpg', 'tauntaun');
+new Pic('img/unicorn.jpg', 'unicorn');
+new Pic('img/usb.gif', 'usb');
+new Pic('img/water-can.jpg', 'water-can');
+new Pic('img/wine-glass.jpg', 'wine-glass');
 
 console.log (Pic.allPictures);
 
@@ -75,28 +75,19 @@ function randomPic () {
 
   // random number generator
   var randomIndexOne = Math.floor(Math.random() * Pic.allPictures.length);
-
-  console.log ('index one: ' +randomIndexOne);
-
   var randomIndexTwo = Math.floor(Math.random() * Pic.allPictures.length);
-  console.log ('index two: ' + randomIndexTwo);
-
   var randomIndexThree = Math.floor(Math.random() * Pic.allPictures.length);
-  console.log ('index three: ' + randomIndexThree);
 
   // check if 2nd random number is equal to 1st
-
   while (randomIndexTwo === randomIndexOne) {
     randomIndexTwo = Math.floor(Math.random() * Pic.allPictures.length);
-    console.log ('index two 1st if: ' + randomIndexTwo);
+
     randomIndexThree = parseInt(Math.floor(Math.random() * Pic.allPictures.length));
-    console.log ('index three 1st if: ' + randomIndexThree);
   }
 
   // check if 3rd random number is equal to 1st or 2nd
   while (randomIndexThree === randomIndexOne || randomIndexThree === randomIndexTwo) {
     randomIndexThree = parseInt(Math.floor(Math.random() * Pic.allPictures.length));
-    console.log ('index three 2nd if: ' + randomIndexThree);
   }
 
   Pic.currentPictures = [];
@@ -114,13 +105,11 @@ function randomPic () {
   Pic.allPictures[randomIndexTwo].countShow ++;
   Pic.allPictures[randomIndexThree].countShow ++;
 
-  console.log('index one count show: ' + Pic.allPictures[randomIndexOne].countShow);
-
   Pic.totalPicCounter ++;
+  console.log('total pic counter: ' + Pic.totalPicCounter);
+
+
 }
-
-
-// track clicks
 
 // render 3 images on page load
 randomPic();
